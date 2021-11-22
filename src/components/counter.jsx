d6cf26212d291d63f933bab0749bd8204621e68b
justyasen <ymanahov@gmail.com>
2021-11-22 21:09:48 +0200

    done for the day, will continue tomorrow

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,48 +3,56 @@ import React, { Component } from "react";
 class Counter extends React.Component {
     //state is an object that includes any data that the component needs
     state = {
-        count: 0,
+        count: 0
     };
-    //encapsulating the method using VSCode refactoring into a standalone method. 
-  render() {
+    // constructor(){
+    //   super(); 
+    //   this.handleIncrement = this.handleIncrement.bind(this); 
+    // }
 
-    React.createElement("div"); 
-    return (
-        //span can be used to render a value dynamically with {}, we have to use this. to reference the current object and then we can point to the expression we want to render
-      <React.Fragment>
-        <span className = { this.changeBtnType() } > {this.formatCount()} </span>
-        <button className = "btn btn-secondary btn-sm"> Increment </button>
-      </React.Fragment>
+    formatCount()
+    {
+        const { count } = this.state.count; 
+        if(count === 0)
+        {
+          return "Zero"; 
+        }
+        else{
+            return count; 
+        }
+    }
+
+    getBadgeClasses()
+    {
+        let classes = "badge m-2 badge-"; 
+        if(this.state.count === 0)
+        {
+          classes += "warning"; 
+        }
+        else
+        {
+          classes += "primary";
+        }
+        return classes; 
+    }
+
+    handleIncrement = (product) => {
+        this.setState({ count: this.state.count + 1}); 
+    }
+    doHandleIncrement = () =>{
+      this.handleIncrement({ id: 1}); 
+    }
+
+  render() {
+    return(
+ <div>
+   <span className = { this.getBadgeClasses() }> { this.formatCount() } </span>
+   <button onClick = { this.doHandleIncrement} className = "btn btn-secondary btn-sm"> 
+   Increment
+   </button>
+ </div>
     );
   }
-  //encapsulated method
-  changeBtnType() {
-    let classes = "badge m-2 badge-";
-    if (this.state.count === 0) {
-      classes += "warning";
-    }
-
-    else {
-      classes += "primary";
-    }
-    return classes;
-  }
-
-//Function that returns value of count. 
-  formatCount()
-  {
-    //destructuring to make code less repetative
-    const { count } = this.state; 
-      if(count === 0)
-      {
-          return "Zero"; 
-      }
-      else
-      {
-          return count; 
-      }
-  }
-
 }
 
 export default Counter;
